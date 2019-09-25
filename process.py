f4498630ea8b89fb16cd35973ffa4b70f8abeb4d
Brad Rowe <brad@rechargeapps.com>
2019-09-25 12:39:33 -0700

    done

--- process.py
+++ process.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import json
+from os import listdir, getcwd
+from os.path import join
 
-
-def process():
-    pass
-
+def process(directory):
+    onlyfiles = [f for f in listdir(directory)]
+    return onlyfiles
 
 if __name__ == '__main__':
-    print('starting!')
-    process()
+    directory = join(getcwd(), "files")
+    print(process(directory))
